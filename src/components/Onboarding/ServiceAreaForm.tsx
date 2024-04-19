915aa904e69f8f144d37d457e7d3531cc12c2d64
wrichardson <wrichardson0521@gmail.com>
2024-04-19 14:06:15 -0500

    Updated service area form

--- src/components/Onboarding/ServiceAreaForm.tsx
+++ src/components/Onboarding/ServiceAreaForm.tsx
@@ -43,7 +43,7 @@ type ServiceArea = {
    state: string;
    postalCode: string;
    country: string;
-   serviceRange: number;
+   serviceRange: string;
 }
 
 
@@ -76,7 +76,7 @@ export default function ContractorForm({ contractorProfile, trades } : { contrac
       state: '',
       postalCode: '',
       country: 'United States',
-      serviceRange: 0
+      serviceRange: ''
    });
 
    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -198,7 +198,7 @@ export default function ContractorForm({ contractorProfile, trades } : { contrac
                      <div className="mt-2">
                         <input
                            onChange={handleChange}
-                           type="number"
+                           type="text"
                            name="serviceRange"
                            id="serviceRange"
                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
