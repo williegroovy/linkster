942cc7ef4159ac89755efa749345c1b21419bf3d
wrichardson <wrichardson0521@gmail.com>
2024-04-29 12:50:30 -0500

    Adding chat to project page

--- src/components/Project/Trade/AddTask.tsx
+++ src/components/Project/Trade/AddTask.tsx
@@ -55,7 +55,7 @@ export default function AddTask({ tradeId, isProjectOwner = false } : { tradeId:
                      leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                      leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                   >
-                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl h-[60vh] sm:p-6">
+                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl min-h-[40vh] sm:p-6">
                         <div>
                            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                               <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
