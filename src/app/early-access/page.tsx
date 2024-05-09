b116599a6db718535968b1f5fd68bdc74e2e6611
wrichardson <wrichardson0521@gmail.com>
2024-05-09 18:34:31 -0500

    Updated beta sign up form

--- src/app/early-access/page.tsx
+++ src/app/early-access/page.tsx
@@ -1,23 +1,26 @@
-import { api } from '~/trpc/server';
-import router from 'next/navigation';
+'use client'
+import { useState } from 'react';
+import { api } from '~/trpc/react';
 
 export default function EarlyAccess({ searchParams } : { searchParams: { plan: string }}) {
+   const [success, setSuccess] = useState(false);
    const plan = searchParams.plan || 'unknown';
 
+   const betaSignup = api.signup.betaSignup.useMutation();
+
    const createBetaSignup = async function(formData: FormData) {
-      'use server'
 
       const email = formData.get('email') as string | null;
 
       if(email) {
-         await api.signup.betaSignup({ email, plan });
-         router.redirect(`/early-access-success`);
+         await betaSignup.mutateAsync({ email, plan });
+         setSuccess(true);
 
       }
 
    }
 
-   return (
+   return !success ? (
       <div className="bg-white py-16 sm:py-24">
          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
@@ -52,6 +55,40 @@ export default function EarlyAccess({ searchParams } : { searchParams: { plan: s
                   className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
                   aria-hidden="true"
                >
+                  <defs>
+                     <radialGradient
+                        id="759c1415-0410-454c-8f7c-9a820de03641"
+                        cx={0}
+                        cy={0}
+                        r={1}
+                        gradientUnits="userSpaceOnUse"
+                        gradientTransform="translate(512 512) rotate(90) scale(512)"
+                     >
+                        <stop stopColor="#7775D6" />
+                        <stop offset={1} stopColor="#E935C1" stopOpacity={0} />
+                     </radialGradient>
+                  </defs>
+               </svg>
+            </div>
+         </div>
+      </div>
+   ) : (
+      <div className="bg-white py-16 sm:py-24">
+         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+               <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  You're on the list!
+               </h2>
+               <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
+                  Be on the look out for our launch email and your 50% off coupon.
+               </p>
+
+               <svg
+                  viewBox="0 0 1024 1024"
+                  className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+                  aria-hidden="true"
+               >
+                  <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
                   <defs>
                      <radialGradient
                         id="759c1415-0410-454c-8f7c-9a820de03641"
